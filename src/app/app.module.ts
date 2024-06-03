488f4adb19dd2be636b2b2a4ff951ac6a4bc52d1
HG092373 <himanshu.gupta1@cerner.com>
2024-06-03 20:48:27 +0530

    made css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,22 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { SimilarProductsComponent } from './components/similar-products/similar-products.component';
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,7 +43,23 @@ import { SimilarProductsComponent } from './components/similar-products/similar-
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   products = [
-    { name: 'Product 1', description: 'Description 1', price: 19.99, image: 'path/to/image1.jpg' },
-    { name: 'Product 2', description: 'Description 2', price: 29.99, image: 'path/to/image2.jpg' },
+    { name: 'Product 1', description: 'Description 1', price: 19.99, image: 'assets/img/product/raw1.jpg' },
+    { name: 'Product 2', description: 'Description 2', price: 29.99, image: 'assets/img/product/raw2.jpg' },
+    { name: 'Product 3', description: 'Description 3', price: 39.99, image: 'assets/img/product/raw3.jpg' },
+    { name: 'Product 4', description: 'Description 4', price: 49.99, image: 'https://artdesigna.com/cdn/shop/files/il_1588xN.4895395148_m8of.jpg?v=1707390902&width=1080' },
+    { name: 'Product 5', description: 'Description 5', price: 59.99, image: 'https://www.thespruce.com/thmb/6nEWAh2KzMGPUmnd5sGCAisO2MM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/diy-moss-art-6753219-01-feb81121729f42568f4ea0c4376b4d3f.jpg' },
+    { name: 'Product 6', description: 'Description 6', price: 59.99, image: 'https://i.etsystatic.com/23378118/r/il/be28f6/5019453544/il_fullxfull.5019453544_bp1z.jpg' },
     // Add more products as needed
   ];
 
