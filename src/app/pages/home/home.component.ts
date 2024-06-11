4c26b399715e455fe8b902d6396df779e9b9133a
HG092373 <himanshu.gupta1@cerner.com>
2024-06-11 20:06:56 +0530

    added product page content

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit{
 
   carousels = [
-    { name: 'Carousel 1', description: 'Description 1', image: 'assets/img/home/carousel/carousel-1.jpg' , bgColor:"rgb(244 244 246)"},
-    { name: 'Carousel 2', description: 'Description 2', image: 'assets/img/home/carousel/carousel-2.jpg' , bgColor:"rgb(208 202 203)"},
-    { name: 'Carousel 3', description: 'Description 3', image: 'assets/img/home/carousel/carousel-3.jpg' , bgColor:"rgb(236 232 224)"},
-    { name: 'Carousel 4', description: 'Description 4', image: 'assets/img/home/carousel/carousel-4.jpg' , bgColor:"rgb(214 211 208)"}
+    { name: 'Live Art for Your Living Space', description: 'Enhance your living environment with our living moss art, bringing a dynamic and lush touch of nature to your walls. #MossMagic', image: 'assets/img/home/carousel/carousel-1.jpg' , bgColor:"rgb(244 244 246)"},
+    
+    { name: 'Transform Your Walls with Moss Masterpieces', description: 'Elevate your decor with our stunning moss masterpieces, perfect for adding a natural and artistic flair to any room.', image: 'assets/img/home/carousel/carousel-2.jpg' , bgColor:"rgb(208 202 203)"},
+    
+    { name: 'Sustainable Beauty, One Moss at a Time', description: 'Discover the sustainable beauty of moss art, handcrafted with care to create stunning pieces that are as eco-friendly as they are beautiful.', image: 'assets/img/home/carousel/carousel-3.jpg' , bgColor:"rgb(236 232 224)"},
+    
+    { name: 'Green Artistry, Naturally Yours', description: 'Embrace the charm of nature with our bespoke moss art, crafted to infuse your home with the vibrant essence of greenery.', image: 'assets/img/home/carousel/carousel-4.jpg' , bgColor:"rgb(214 211 208)"}
   ];
 
   trends = [{ name: 'Product 1', description: 'Description 1', price: 19.99, image: 'assets/img/product/p1/trend.jpg', rank: 1 },
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -7,13 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   products = [
-    { name: 'Product 1', description: 'Description 1', price: 19.99, image: 'assets/img/product/raw1.jpg' },
-    { name: 'Product 2', description: 'Description 2', price: 29.99, image: 'assets/img/product/raw2.jpg' },
-    { name: 'Product 3', description: 'Description 3', price: 39.99, image: 'assets/img/product/raw3.jpg' },
-    { name: 'Product 4', description: 'Description 4', price: 49.99, image: 'https://artdesigna.com/cdn/shop/files/il_1588xN.4895395148_m8of.jpg?v=1707390902&width=1080' },
-    { name: 'Product 5', description: 'Description 5', price: 59.99, image: 'https://www.thespruce.com/thmb/6nEWAh2KzMGPUmnd5sGCAisO2MM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/diy-moss-art-6753219-01-feb81121729f42568f4ea0c4376b4d3f.jpg' },
-    { name: 'Product 6', description: 'Description 6', price: 59.99, image: 'https://i.etsystatic.com/23378118/r/il/be28f6/5019453544/il_fullxfull.5019453544_bp1z.jpg' },
-    // Add more products as needed
+    { name: 'Panno Wood Slices', category : 'Moss Wall Art', description: 'Description 1', actualprice: 59.99 ,price: 49.99, image: 'assets/img/product/p1/trend.jpg', stock: 4 },
+    { name: 'Bee Art Deco', category : 'Moss Sculptures',description: 'Description 2', actualprice: 45.99, price: 40.99, image: 'assets/img/product/p2/trend.jpg', stock: 10 },
+    { name: 'Ferm Moss', category : 'Moss Wall Art',description: 'Description 3', actualprice: 80.99, price: 65.99, image: 'assets/img/product/p3/trend.jpg', stock: 8 },
+    { name: 'Ferm Moss', category : 'Moss Wall Art',description: 'Description 3', actualprice: 40.99, price: 35.99, image: 'assets/img/product/p4/trend.jpg', stock: 8 },
+    { name: 'Ferm Moss', category : 'Moss Wall Art',description: 'Description 3', actualprice: 80.99, price: 65.99, image: 'assets/img/product/p5/trend.jpg', stock: 8 },
+    { name: 'Ferm Moss', category : 'Moss Wall Art',description: 'Description 3', actualprice: 100.99, price: 95.99, image: 'assets/img/product/p6/trend.jpg', stock: 8 },
   ];
 
   constructor() { }
